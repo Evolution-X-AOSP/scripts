9572481437bc5a5bfb1f88338cecf5700b1631aa
Michael Bestas <mkbestas@lineageos.org>
2022-08-26 22:46:25 +0300

    pixel: Fix build-desc-fingerprint.sh not commiting raviole changes
    Change-Id: I58dd7978e2f243d9295fcb085f8ae49629a38157
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- pixel/build-desc-fingerprint.sh
+++ pixel/build-desc-fingerprint.sh
@@ -74,7 +74,8 @@ main() {
     (
       local dv="${vars_path}/${d}"
       source "${dv}"
-      cd "${top}/device/google/${d}"
+      local dir="$(ls ${top}/device/google/*/evolution_${d}.mk | sed s#/evolution_${d}.mk##)"
+      cd "${dir}"
       if [[ -n "$(git status --porcelain)" ]]; then
         git commit -a -m "Update fingerprint/build description from ${build_id}"
       fi
